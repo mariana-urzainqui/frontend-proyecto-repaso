669c78743137b153954645f72f4e2b848a605d22
mariana.urzainqui <marianaurzainqui@gmail.com>
2024-11-22 10:46:50 -0300

    verify email

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import HomeScreen from './Screens/HomeScreen/HomeScreen'
 import CreateProduct from './Screens/CreateProduct/CreateProduct'
 import DetailProduct from './Screens/DetailProduct/DetailProduct'
 import ProtectedRoute from './Components/ProtectedRoute'
+import VerifyEmail from './Screens/VerifyMail/VerifyMail'
 
 function App() {
   return (
@@ -18,7 +19,7 @@ function App() {
         <Route path='/register' element={<Register />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/reset-password/:reset_token' element={<ResetPassword />} />
-        <Route path="/verify/:verificationToken" element={<VerifyEmail />} />
+        <Route path="/verify/:verificationToken" element={<VerifyEmail/>} />
         <Route element={<ProtectedRoute />}>
           <Route path='/home' element={<HomeScreen />} />
           <Route path='/product/new' element={<CreateProduct />} />
